5be5e432e9c4b8568d89e47e61a61024fe87f5dd
Shannon Xiao <sxiao1@stanford.edu>
2025-04-30 14:03:41 -0700

    parsed tract list from poverty csv

--- geocode_v4.py
+++ geocode_v4.py
@@ -9,6 +9,8 @@ poverty_data = pd.read_csv(
     dtype={'Tract': str}
 )
 poverty_tracts = set(poverty_data['Tract'])
+poverty_census_tracts = {tract[-6:] for tract in poverty_tracts}
+poverty_counties = {tract[-9:-6] for tract in poverty_tracts} # county code of the poverty tract, doesn't mean the county itself is in poverty
 
 # === STEP 2: Read addresses from addresses.txt ===
 with open("test.txt", "r") as f:
@@ -42,6 +44,7 @@ with open(input_file, "rb") as f:
         data=data
     )
 
+
 # === STEP 5: Check response ===
 # if not response.ok or not response.headers['Content-Type'].startswith("text/csv"):
 if not response.ok:
@@ -57,7 +60,6 @@ for row in results:
     if len(row) < 10:
         print(f"Incomplete or no match: {row}")
         continue
-
     index = row[0]
     input_address = lines[int(index)]
     geoid = row[9]  # Full GEOID
@@ -72,10 +74,22 @@ for row in results:
     # print(f"  County FIPS: {county_fips}")
     # print(f"  Census Tract: {tract_code}")
 
-    if geoid in poverty_tracts:
-        print(f"{input_address}  → This tract IS in a persistent poverty area.\n")
+    # if geoid in poverty_tracts:
+    #     print(f"{input_address}  → This tract IS in a persistent poverty area.\n")
+    # else:
+    #     print(f"{input_address}  → This tract is NOT in a persistent poverty area.\n")
+    county_code = row[-3]
+    tract_code = row[-2]
+    print(f"{input_address}: ")
+
+    if tract_code in poverty_census_tracts:
+        print("This tract IS in a persistent poverty area.")
     else:
-        print(f"{input_address}  → This tract is NOT in a persistent poverty area.\n")
+        print("This tract is NOT in a persistent poverty area.")
+    if county_code in poverty_counties:
+        print("This county IS in a persistent poverty area.\n")
+    else:
+        print("This county is NOT in a persistent poverty area.\n")
 
 # === Optional: Clean up the temp file ===
 os.remove(input_file)
